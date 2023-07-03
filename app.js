f69337d97434341794e2f7fd7fab5cc54b12515e
SilentCode1003 <j0s3ph0r3nc10@gmail.com>
2023-07-03 14:37:19 +0800

    Added View page for Employee Education, Work Exp, Reference, Gov ID & Employment Details

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
+const cors = require("cors");
 var logger = require("morgan");
+const bodyParser = require("body-parser");
 
 const mysql = require("./routes/repository/hrmisdb");
 
@@ -28,10 +30,14 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "25mb" }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(cors());
+
+
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
